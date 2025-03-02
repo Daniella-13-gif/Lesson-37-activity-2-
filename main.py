006b3719d29b02bacd79c28c2052c93f51e11bb9
Daniella-13-gif <ngozihomestore@gmail.com>
2025-03-02 13:32:47 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+num1 = 30
+num2 = 2
+
+print("Number 1:", num1)
+print("Number 2:", num2)
+print("Addition:", num1+num2)
+print("Subtraction:", num1-num2)
+print("Multiplication:", num1*num2)
+print("Division:", num1/num2)
